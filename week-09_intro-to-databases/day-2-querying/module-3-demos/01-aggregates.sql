2a6e53e7d6ca4874c996d3c94078d241ea1934f7
Gregor Dzierzon <gregor@fullstackdevs.net>
2026-06-02 16:25:24 -0600

    added aggregation demos

--- week-09_intro-to-databases/day-2-querying/module-3-demos/01-aggregates.sql
+++ week-09_intro-to-databases/day-2-querying/module-3-demos/01-aggregates.sql
@@ -1,30 +1,56 @@
+USE sakila;
 
 -- How many customers are in the database?
+SELECT COUNT(*)
+    , COUNT(address2)
+FROM address;
+
+SELECT *
+FROM address;
 
 
 -- How many films are rated PG?
+SELECT COUNT(*) AS total_count
+FROM film
+WHERE rating = 'PG'
+;
 
 
 -- How many distinct ratings exist?
+SELECT COUNT(DISTINCT rating) AS distinct_rating_count
+FROM film
+;
 
 
 -- Total revenue collected: payment table
+SELECT SUM(amount)
+FROM payment;
 
 
 
 -- Total revenue from customer 1:
+SELECT SUM(amount)
+FROM payment
+WHERE customer_id = 1
+;
 
 
 
 -- Average payment amount:
+SELECT SUM(amount) AS total_paid
+    , SUM(amount) / COUNT(amount) AS calculated_average
+    , AVG(amount) as average_paid
+FROM payment;
 
 
 
 -- Average film length:
-
+SELECT AVG(length) AS average_film_length
+FROM film;
 
 -- Round the result: to 2 decimal places
-
+SELECT ROUND(AVG(length), 2) AS average_film_length
+FROM film;
 
 -- Shortest movie:
 
@@ -40,8 +66,8 @@
 
 -- Multiple Aggregates Together
 
--- SELECT COUNT(*)      AS FilmCount
---      , AVG(length)  AS AvgLength
---      , MIN(length)  AS ShortestFilm
---      , MAX(length)  AS LongestFilm
--- FROM film;
+SELECT COUNT(*)      AS FilmCount
+     , ROUND(AVG(length),0)  AS AvgLength
+     , MIN(length)  AS ShortestFilm
+     , MAX(length)  AS LongestFilm
+FROM film;
